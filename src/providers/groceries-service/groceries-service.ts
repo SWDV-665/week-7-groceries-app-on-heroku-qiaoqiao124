eaeeb339b9edd240ed2257017f0a63fd91a9bbe5
Qiaoqiao Liu <qliu1@live.maryville.edu>
2022-08-11 14:57:09 -0700

    make it better

--- src/providers/groceries-service/groceries-service.ts
+++ src/providers/groceries-service/groceries-service.ts
@@ -14,7 +14,7 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class GroceriesServiceProvider {
   // baseURL = "http://localhost:8080";
-  baseURL = "https://groceries-server-q.herokuapp.com/";
+  baseURL = "https://groceries-server-q.herokuapp.com";
 
   items = [];
   dataModified: Observable<boolean>;
